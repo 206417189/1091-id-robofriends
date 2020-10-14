26e1377b3fca07de13098ddb3b70fb6e5f702da8
206417189 <206417189@gms.tku.edu.tw>
2020-10-14 19:23:28 +0800

    Updates

---
+++ precache-manifest.8b2ce5539a09ef6892c4421bc6dada00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31fc91865c0b5c96f3c09bc4351c8c0",
+    "url": "/1091-id-robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-id-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "85b9cf611216e88093fc",
+    "url": "/1091-id-robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-id-robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1091-id-robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b9cf611216e88093fc",
+    "url": "/1091-id-robofriends/static/js/main.285b0b7f.chunk.js"
+  },
+  {
+    "revision": "a51abbcf008854cbf355",
+    "url": "/1091-id-robofriends/static/js/runtime-main.7f4955e0.js"
+  }
+]);
